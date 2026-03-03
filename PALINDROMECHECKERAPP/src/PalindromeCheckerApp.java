8bfa960f78d994a9b3644c8fe87d862a16a9c921
VARSHINI00708 <varsh07ms@gmail.com>
2026-03-03 15:15:31 +0530

    UC2: Print a Hardcoded Palindrome Result

--- PALINDROMECHECKERAPP/src/PalindromeCheckerApp.java
+++ PALINDROMECHECKERAPP/src/PalindromeCheckerApp.java
@@ -1,10 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
-    private static final String Welcome_msg = "Welcome to the Palindrome Checker Management System";
-    private static final String Version = "1.0";
+
 
     static void main(String[] args){
-        System.out.println(Welcome_msg);
-        System.out.println("Version: "+Version);
-        System.out.println("System initialized successfully");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the text: ");
+        String text = input.nextLine();
+        text = text.toLowerCase(Locale.ROOT);
+        Boolean flag = true;
+        for(int i = 0; i<text.length()/2; i++ ){
+            if(text.charAt(i) != text.charAt(text.length()-i-1)){
+                flag = false;
+                break;
+            }
+        } System.out.println("Is it a palindrome? "+flag);
     }
 }
