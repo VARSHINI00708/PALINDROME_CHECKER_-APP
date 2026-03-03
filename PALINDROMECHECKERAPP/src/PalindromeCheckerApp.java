a33ec4614b134322c33d128b1e44e9398a9c7515
VARSHINI00708 <varsh07ms@gmail.com>
2026-03-03 16:33:07 +0530

    UC8: Linked List Based Palindrome Checker

--- PALINDROMECHECKERAPP/src/PalindromeCheckerApp.java
+++ PALINDROMECHECKERAPP/src/PalindromeCheckerApp.java
@@ -1,23 +1,86 @@
 import java.util.*;
 
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node createList(String str) {
+        Node head = null, tail = null;
+
+        for (char ch : str.toCharArray()) {
+            Node newNode = new Node(ch);
+
+            if (head == null) {
+                head = tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+        return head;
+    }
+    public static boolean isPalindrome(Node head) {
+        if (head == null || head.next == null) return true;
+
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+
+        Node secondHalf = reverse(slow);
+
+
+        Node firstHalf = head;
+        Node temp = secondHalf;
+
+        while (temp != null) {
+            if (firstHalf.data != temp.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            temp = temp.next;
+        }
+
+        return true;
+    }
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the text: ");
         String text = input.nextLine();
         text = text.toLowerCase(Locale.ROOT);
-        boolean flag = true;
-        Deque<Character> deque = new ArrayDeque<>();
-        for(char c: text.toCharArray()){
-            deque.add(c);
+
+        Node head = createList(text);
+
+        if (isPalindrome(head)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
         }
-        while(deque.size()>1){
-            if(!deque.removeFirst().equals(deque.removeLast())){
-                flag = false;
-                break;
-            }
-        }
-        System.out.println((flag)?"It's a palindrome":"Not a palindrome");
     }
 }
 
