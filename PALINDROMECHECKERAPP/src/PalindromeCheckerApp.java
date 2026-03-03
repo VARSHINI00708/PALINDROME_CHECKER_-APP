f7a27b9956d5e68c5edd0d2ffcd25d46d4bf5eb5
VARSHINI00708 <varsh07ms@gmail.com>
2026-03-03 16:15:17 +0530

    UC6: Queue + Stack Based Palindrome Check

--- PALINDROMECHECKERAPP/src/PalindromeCheckerApp.java
+++ PALINDROMECHECKERAPP/src/PalindromeCheckerApp.java
@@ -1,26 +1,26 @@
-import java.util.Locale;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-                Scanner input = new Scanner(System.in);
-                System.out.print("Enter the text: ");
-                String text = input.nextLine();
-                text = text.toLowerCase(Locale.ROOT);
-                boolean flag = true;
-                Stack<Character> stack = new Stack<>();
-                for( char c: text.toCharArray()){
-                    stack.push(c);
-                }
-                for(char c: text.toCharArray()){
-                    if(!stack.pop().equals(c)){
-                        flag = false;
-                        break;
-                    }
-                }
-                System.out.println((flag)?"It's a palindrome":"Not a palindrome");
-            };
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the text: ");
+        String text = input.nextLine();
+        text = text.toLowerCase(Locale.ROOT);
+        boolean flag = true;
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+        for( char c: text.toCharArray()){
+            stack.push(c);
+            queue.add(c);
+        }
+        for ( int i=0; i<text.length(); i++ ){
+            if(!stack.pop().equals(queue.remove())){
+                flag = false;
+                break;
+            }
+        }
+        System.out.println((flag)?"It's a palindrome":"Not a palindrome");
+    }
 
 }
 
